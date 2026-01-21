886bce4819cd32f94fd15b633260d7322ee0b1d9
fabdul-rah <124473430+fabdul-rah@users.noreply.github.com>
2026-01-21 12:15:54 -0600

    Target1: added a basic FastAPI application with HTMLResponse and PlainTextResponse
    Additionally:
    
    @app.get("/")
    @app.get("/health")
    @app.get("/health")
    @app.get("/api/{func}")

--- project/nodes/target1/app/main.py
+++ project/nodes/target1/app/main.py
@@ -1,1 +1,22 @@
-import FastAPI from fastapi
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import HTMLResponse, PlainTextResponse
+import time
+
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return "<html><body><h1>Target1 Machine actings as a victim to attacking machines</h1></body></html>"
+
+@app.get("/assets/{name}")
+def asset(name: str):
+    return PlainTextResponse(f"asset:{name}")
+
+@app.get("/health")
+def health():
+    return {"ok": True}
+
+@app.get("/api/{func}")
+def api_func(func: str):
+    return {"func": func, "result": "ok"}
