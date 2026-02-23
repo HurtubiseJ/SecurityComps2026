f882a30661fecc097db9ca9b24b92fcafce8a350
John Hurtubise <jhurtubise23@gmail.com>
2026-02-23 17:04:12 -0600

    Prom IPs mod, Proxy add HOST injection

--- project/nodes/proxy/nginx/reload_conf.sh
+++ project/nodes/proxy/nginx/reload_conf.sh
@@ -4,6 +4,7 @@ set -e
 CONFIG_JSON="/MASTER_CONFIG.json"
 
 PORT=$(jq -r '.port' $CONFIG_JSON)
+HOST=$(jq -r '.host' $CONFIG_JSON)
 RATE_LIMIT=$(jq -r '.custom_config.rate_limit_rate' $CONFIG_JSON)
 FORWARD_HOST=$(jq -r '.forward_host' $CONFIG_JSON)
 FORWARD_PORT=$(jq -r '.forward_port' $CONFIG_JSON)
@@ -14,12 +15,12 @@ READ_TIMEOUT=$(jq -r '.custom_config.read_timeout' $CONFIG_JSON)
 SEND_TIMEOUT=$(jq -r '.custom_config.send_timeout' $CONFIG_JSON)
 
 export RATE_LIMIT RATE_LIMIT_STATUS CONN_LIMIT_STATUS \
-       FORWARD_HOST FORWARD_PORT PORT \
+       FORWARD_HOST FORWARD_PORT PORT HOST \
        MAX_CONNECTIONS BURST \
        CONNECTION_TIMEOUT READ_TIMEOUT SEND_TIMEOUT
 
 envsubst '$RATE_LIMIT $RATE_LIMIT_STATUS $CONN_LIMIT_STATUS \
-$FORWARD_HOST $FORWARD_PORT $PORT \
+$FORWARD_HOST $FORWARD_PORT $PORT $HOST \
 $MAX_CONNECTIONS $BURST \
 $CONNECTION_TIMEOUT $READ_TIMEOUT $SEND_TIMEOUT' \
 < /etc/nginx/nginx.conf.template \
